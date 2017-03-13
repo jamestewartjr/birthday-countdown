9535ae90b84bb002d7357e49d2a89e8ac0932cc0
dsteplight <dsteplight@gmail.com>
2017-03-12 21:25:12 -0400

    Updating Texting

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>The DoT Birthday Countdown App</h2>
+          <h2>Birthday Countdown Demo</h2>
         </div>
             <Container>
             <Form>
